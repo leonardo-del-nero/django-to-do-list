c1b08d0fb9d41798a0d4ff595b351fcbba623eb2
leonardo-del-nero <shidejavu4@gmail.com>
2025-09-14 22:23:50 -0300

    Update ToDoList view to display list and item
    Modified the index view to accept a list name parameter and display the corresponding ToDoList and its first item. Updated URL patterns to route based on list name and removed the unused v1 view. Added db.sqlite3 to .gitignore for better local development.

--- main/models.py
+++ main/models.py
@@ -7,12 +7,12 @@ class ToDoList(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+
 class Item(models.Model):
     todolist = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
     text = models.CharField(max_length=300)
     complete = models.BooleanField()
 
     def __str__(self):
-        return self.text
-    
+        return self.text 
--- main/views.py
+++ main/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("<h1>tech with tim</h1>")
+def index(respoonse, name):
+    ls = ToDoList.objects.get(name=name)
+    item = ls.item_set.get(id = 1)
+    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(item.text)))
 
-def v1(request):
-    return HttpResponse("<h1>View 1!  </h1>")
+
