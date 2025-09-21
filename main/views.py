b630ed7d42328e8839ee15676bb8a9f48b4715f2
leonardo-del-nero <shidejavu4@gmail.com>
2025-09-21 14:30:52 -0300

    upgrade

--- main/views.py
+++ main/views.py
@@ -7,8 +7,10 @@ from .models import ToDoList, Item
 def index(respoonse, id):
     
     ls = ToDoList.objects.get(id=id)
-    return render(respoonse, 'main/base.html', {"name": ls.name})
+    return render(respoonse, 'main/list.html', {"ls":ls})
 
 def home(response):
-    return render(response, 'main/home.html', {"name": "teste"})
+    return render(response, 'main/home.html', {})
+
+
 
